72d210361cbf6a6a437734ba1b9281aada99e0b8
mafutka <mafutka@gmail.com>
2026-05-03 20:16:20 +0300

    form created

--- src/app/components/test/test.ts
+++ src/app/components/test/test.ts
@@ -1,40 +1,21 @@
 
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-import { TodoService } from '../../services/todo';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { email } from '@angular/forms/signals';
 
 @Component({
   selector: 'app-test',
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, FormsModule, ReactiveFormsModule ],
    standalone: true,
   templateUrl: './test.html',
   styleUrls: ['./test.scss'],
 })
-export class TestComponent implements OnInit {
-  newTask: string = '';
-  tasks: string[] = []
+export class TestComponent {
+ complexForm = new FormGroup({
+  name:new FormControl('', [Validators.required , Validators.minLength(2)]),
+  email: new FormControl('', [Validators.required, Validators.email])
+ })
 
-  constructor () {} 
-
-  private todoService = inject(TodoService);
-
-  ngOnInit(): void {
-    this.tasks = this.todoService.getTasks()
-  }
-
-  addTask() {
-    if(this.newTask.trim() !== '')
-      this.todoService.addTask(this.newTask.trim());
-    this.newTask = '';
-    this.updateTasks()
-  }
-
-  removeTask(index: number){
-this.todoService.removeTask(index)
-this.updateTasks()
-  }
-  private updateTasks(){
-    this.tasks = this.todoService.getTasks();
-  }
+ onSubmit() {}
 }
