5206d6f43befbd741e708c24d1b99c5090dec9be
mafutka <mafutka@gmail.com>
2026-05-05 15:17:37 +0300

    data-created

---
+++ src/app/data/questions.ts
@@ -0,0 +1,20 @@
+export const QUESTIONS = [
+  {
+    id: 1,
+    question: 'What is Angular?',
+    answer: 'Angular is a frontend framework...',
+    category: 'angular'
+  },
+  {
+    id: 2,
+    question: 'What is closure?',
+    answer: 'Closure is a function...',
+    category: 'javascript'
+  },
+  {
+    id: 3,
+    question: 'What is RxJS?',
+    answer: 'RxJS is a reactive library...',
+    category: 'rxjs'
+  }
+];
