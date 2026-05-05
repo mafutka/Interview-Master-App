012075b9b53397b73a1b8aada1651f96c6dd07ef
mafutka <mafutka@gmail.com>
2026-05-05 16:52:15 +0300

    app-modified

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,20 +1,10 @@
-import { Component, signal } from '@angular/core';
-import { TestComponent } from './components/test/test';
-import { List } from './components/list/list';
-
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [ TestComponent, List],
+  imports: [RouterOutlet], 
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
-export class AppComponennt {
-  protected readonly title = signal('angular-interview-app');
-
-  messageFromChild =''
-  getMassageFromChild(massage: string) {
-    this.messageFromChild = massage
-
-  }
-}
+export class AppComponennt {}
--- src/app/components/layout/layout.ts
+++ src/app/components/layout/layout.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { Sidebar } from '../sidebar/sidebar';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-layout',
-  imports: [],
+  imports: [Sidebar, RouterOutlet],
   templateUrl: './layout.html',
   styleUrl: './layout.scss',
 })
